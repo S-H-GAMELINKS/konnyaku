0c491a8c8bf7fdedd21ed502b3b28de3156ce12d
S-H-GAMELINKS <gamelinks007@gmail.com>
2021-01-28 13:16:54 +0900

    fix test

--- test/test_konnyaku.rb
+++ test/test_konnyaku.rb
@@ -1,13 +1,10 @@
 # frozen_string_literal: true
 
 require "test_helper"
+require "konnyaku"
 
 class TestKonnyaku < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Konnyaku::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
   end
 end
